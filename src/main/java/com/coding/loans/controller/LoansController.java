49149c3d5bb48c9bee1c75cbac7b34b1ad418730
viveksinghrawat <vivek.rawat187@gmail.com>
2025-08-11 22:11:47 +0530

    updated

--- src/main/java/com/coding/loans/controller/LoansController.java
+++ src/main/java/com/coding/loans/controller/LoansController.java
@@ -33,7 +33,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
-@Validated
 public class LoansController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
